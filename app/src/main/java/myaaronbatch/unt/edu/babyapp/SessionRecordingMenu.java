b324009c3f40fa0752efbcac5f89b05f99380f0b
Aaron B <aarondbatch@gmail.com>
2018-03-15 22:50:08 -0500

    Renamed the "sessionType" file, tested and finished the SessionRecordingMenu, and started on the StartOrEndSessionMenu

--- app/src/main/java/myaaronbatch/unt/edu/babyapp/SessionRecordingMenu.java
+++ app/src/main/java/myaaronbatch/unt/edu/babyapp/SessionRecordingMenu.java
@@ -1,19 +1,21 @@
 package myaaronbatch.unt.edu.babyapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+// Class that represents the "Session Recording Menu"
 public class SessionRecordingMenu extends AppCompatActivity
 {
-    Button backButton; // Btn which represents the back button on the view
+    Button backBtn; // Btn which represents the back button on the view
     Button feedingSessionBtn; // Btn which represents the feeding session button on the view
     Button sleepingSessionBtn; // Btn which represents the sleeping session button on the view
     Button wasteSessionBtn; // Btn which represents the waste session button on the view
     Button medicationSessionBtn; // Btn which represents the medication session button on the view
 
+    // On creation of this activity, set up all of the buttons on the screen (as well as their onClick methods) as well as other essential things
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,7 +23,7 @@ public class SessionRecordingMenu extends AppCompatActivity
         setContentView(R.layout.activity_session_recording_menu);
 
         // Get references to buttons on the activity view
-        backButton = (Button) findViewById(R.id.backBtn);
+        backBtn = (Button) findViewById(R.id.backBtn);
         feedingSessionBtn = (Button) findViewById(R.id.feedingSessionBtn);
         sleepingSessionBtn = (Button) findViewById(R.id.sleepingSessionBtn);
         wasteSessionBtn = (Button) findViewById(R.id.wasteSessionBtn);
@@ -31,7 +33,7 @@ public class SessionRecordingMenu extends AppCompatActivity
         final Intent goToStartEndSessionMenuIntent = new Intent(getApplicationContext(), StartOrEndSessionMenu.class);
 
         // When backButton is tapped, send the user to the previous view that they were on
-        backButton.setOnClickListener(new View.OnClickListener()
+        backBtn.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
@@ -47,7 +49,7 @@ public class SessionRecordingMenu extends AppCompatActivity
             public void onClick(View view)
             {
                 // Send what type of session the user chose to the next menu
-                goToStartEndSessionMenuIntent.putExtra("Session_Type", "Feeding Session");
+                goToStartEndSessionMenuIntent.putExtra("Session_Type", "FEEDING_SESSION");
 
                 // Go to the menu the user chose
                 startActivity(goToStartEndSessionMenuIntent);
@@ -61,7 +63,7 @@ public class SessionRecordingMenu extends AppCompatActivity
             public void onClick(View view)
             {
                 // Send what type of session the user chose to the next menu
-                goToStartEndSessionMenuIntent.putExtra("Session_Type", "Sleeping Session");
+                goToStartEndSessionMenuIntent.putExtra("Session_Type", "SLEEPING_SESSION");
 
                 // Go to the menu the user chose
                 startActivity(goToStartEndSessionMenuIntent);
@@ -75,21 +77,21 @@ public class SessionRecordingMenu extends AppCompatActivity
             public void onClick(View view)
             {
                 // Send what type of session the user chose to the next menu
-                goToStartEndSessionMenuIntent.putExtra("Session_Type", "Waste Session");
+                goToStartEndSessionMenuIntent.putExtra("Session_Type", "WASTE_SESSION");
 
                 // Go to the menu the user chose
                 startActivity(goToStartEndSessionMenuIntent);
             }
         });
 
-        // When the sleepingSessionBtn is tapped, send the user to the Start/End session menu
+        // When the medicationSessionBtn is tapped, send the user to the Start/End session menu
         medicationSessionBtn.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
                 // Send what type of session the user chose to the next menu
-                goToStartEndSessionMenuIntent.putExtra("Session_Type", "Medication Session");
+                goToStartEndSessionMenuIntent.putExtra("Session_Type", "MEDICATION_SESSION");
 
                 // Go to the menu the user chose
                 startActivity(goToStartEndSessionMenuIntent);
--- app/src/main/java/myaaronbatch/unt/edu/babyapp/StartOrEndSessionMenu.java
+++ app/src/main/java/myaaronbatch/unt/edu/babyapp/StartOrEndSessionMenu.java
@@ -1,35 +1,75 @@
 package myaaronbatch.unt.edu.babyapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+// Class which represents the "Start/End Session Menu"
 public class StartOrEndSessionMenu extends AppCompatActivity
 {
-    Button backBtn;
-    Button startOrEndBtn;
-    TextView titleTextView;
-    TextView testTextView;
+    Button backBtn; // Button which will take the user to the previous screen
+    Button startOrEndBtn; // Button which will start a session or end a session (depending on what it is displaying at that time)
+    TextView titleTextView; // Textview which represents the title of the activity
+    TextView testTextView; // Textview which exists for testing purposes
+    String sessionType; // String representing what kind of session the user would like to potentially start or end
+    Boolean startPushed = false; // Boolean representing whether the user has pressed
+    String menuName; // String representing the name if the menu that the user came from before they got to this menu
+    Intent sentIntent;
 
+    Integer index;
+
+    // On activity creation, set up all of the buttons, textviews, and other vital elements of the activity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_or_end_session_menu);
 
+        // Get references to the UI elements on the activity
         backBtn = (Button) findViewById(R.id.backBtn);
         startOrEndBtn = (Button) findViewById(R.id.startOrEndBtn);
         testTextView = (TextView) findViewById(R.id.testTextView);
 
-        if(getIntent().hasExtra("Session_Type"))
+        if(getIntent().hasExtra("MenuName"))
         {
-            String test = getIntent().getExtras().getString("Session_Type");
-
-            testTextView.setText(test);
+            menuName = getIntent().getExtras().getString("MenuName");
         }
 
+        if(menuName == "SessionRecordingMenu")
+        {
+            startPushed = false;
+
+            // If the previous view has sent us some information under the data-title "Session_Type", then use that info to find out what type of session the user
+            // wanted to start
+            if (getIntent().hasExtra("Session_Type"))
+            {
+                // Get intent
+                sentIntent = getIntent();
+
+                // Get the session type that was sent in the string message
+                sessionType = sentIntent.getExtras().getString("Session_Type");
+
+                testTextView.setText(sessionType);
+            }
+
+        }
+
+        else if(menuName == "SessionViewingMenu")
+        {
+            if(getIntent().hasExtra("index"))
+            {
+                sentIntent = getIntent();
+
+                index = sentIntent.getExtras().getInt("index");
+            }
+
+            //if(getIntent().hasExtra(""))
+        }
+
+        // If the back button is selected, then go to the previous view that the user was on
         backBtn.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -38,5 +78,7 @@ public class StartOrEndSessionMenu extends AppCompatActivity
                 finish();
             }
         });
+
+
     }
 }
