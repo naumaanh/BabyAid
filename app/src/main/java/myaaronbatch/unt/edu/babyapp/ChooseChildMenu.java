600530fa2152ead4475b80b8b64a1026e3bac41e
Nathan Cramer <Spiritofpower24@gmail.com>
2018-04-06 18:53:37 -0500

    Added Choosing child menu
    Added menu for choosing a child, made that the main activity, and also added empty activities for adding and removing children. Haven't tested it properly yet, need adding child functionality for that.

---
+++ app/src/main/java/myaaronbatch/unt/edu/babyapp/ChooseChildMenu.java
@@ -0,0 +1,79 @@
+package myaaronbatch.unt.edu.babyapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import AppDataStructures.Child;
+import AppDataStructures.Settings;
+
+public class ChooseChildMenu extends AppCompatActivity {
+    Button rmvBtn;
+    Button addBtn;
+    LinearLayout childList;
+    LinearLayout.LayoutParams params;
+    Settings options;
+    ArrayList<Child> kids;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_child_menu);
+
+        options = Settings.getInstance();
+        kids = Child.getChildren();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+
+        rmvBtn = findViewById(R.id.removeChildBtn);
+        addBtn = findViewById(R.id.addChildBtn);
+        childList = findViewById(R.id.childlistLayout);
+        params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        rmvBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RemoveChildMenu.class);
+                startActivity(intent);
+            }
+        });
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddChildMenu.class);
+                startActivity(intent);
+            }
+        });
+
+        for (int i = 0; i < kids.size(); i++)
+        {
+            Button temp = new Button(this);
+            temp.setId(i);
+            if (kids.get(i).firstName.isEmpty())
+            {
+                temp.setText("DEBUG CHILD");
+            }
+            else
+            {
+                temp.setText(kids.get(i).firstName + ", born " + sdf.format(kids.get(i).dateOfBirth.getTime()));
+            }
+
+            temp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    options.childIndex = view.getId();
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+}
