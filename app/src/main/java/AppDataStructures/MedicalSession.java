ca05d01259c314092b34d1051e24f9e289124162
Nathan Cramer <Spiritofpower24@gmail.com>
2018-05-01 13:12:35 -0500

    Added Dropbox SDK and XStream aliases
    Hopefully this works this time

--- app/src/main/java/AppDataStructures/MedicalSession.java
+++ app/src/main/java/AppDataStructures/MedicalSession.java
@@ -1,12 +1,17 @@
 package AppDataStructures;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
 /**
  * Created by aaronbatch on 3/5/18.
  */
-
+@XStreamAlias("MSESS")
 public class MedicalSession extends Session {
+    @XStreamAlias("MTYPE")
     public String type = "";
+    @XStreamAlias("MDOSAGE")
     public int dosage;
+    @XStreamAlias("MWAY")
     public medGiven way;
 
     public MedicalSession() {
--- app/src/main/java/AppDataStructures/Settings.java
+++ app/src/main/java/AppDataStructures/Settings.java
@@ -1,17 +1,23 @@
 package AppDataStructures;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
 /**
  * Created by aaronbatch on 3/5/18.
  */
-
+@XStreamAlias("SETTINGS")
 public class Settings {
+    @XStreamAlias("24HTIME")
     public boolean is24HTime;
+    @XStreamAlias("DARKTHEME")
     public boolean isDarkTheme;
+    @XStreamAlias("ISMETRIC")
     public boolean isMetric;
-
+    @XStreamAlias("CINDEX")
     public int childIndex = 0;
+    @XStreamAlias("NUMCHILDREN")
     public Integer numberOfChildren = 0;
-
+    @XStreamAlias("STYLEINT")
     public int styleInt;
 
     public static Settings instance;
