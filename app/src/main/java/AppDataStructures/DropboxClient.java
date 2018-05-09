dcf43f3d236d143b59fc6ca22b17c569c143c1cd
Nathan Cramer <Spiritofpower24@gmail.com>
2018-05-08 23:44:47 -0500

    Turned dropbox DL back on

--- app/src/main/java/AppDataStructures/DropboxClient.java
+++ app/src/main/java/AppDataStructures/DropboxClient.java
@@ -10,7 +10,7 @@ import com.dropbox.core.v2.DbxClientV2;
 public class DropboxClient {
     public static DbxClientV2 getClient(String ACCESS_TOKEN) {
         // Create Dropbox client
-        DbxRequestConfig config = new DbxRequestConfig("dropbox/Apps/BabyAid", "en_US");
+        DbxRequestConfig config = new DbxRequestConfig("dropbox/BabyAid", "en_US");
         DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
         return client;
     }
